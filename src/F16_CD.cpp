018db7efcb03f6d4591054c028905dfc6c5255b5
Colton Acosta <cacost12@asu.edu>
2021-04-11 11:43:36 -0700

    Create Library for F-16 calculations

---
+++ src/F16_CD.cpp
@@ -0,0 +1,18 @@
+/************************************************************* 
+* F16_CD -- Library to contain all data pertaining to the    *
+*           F-16 C/D Aircraft, including aircraft specs,     *
+*           stability derivatives, and cg location analyses  *
+*                                                            * 
+* AUTHOR: Colton Acosta, 1214326892, cacost12@asu.edu        *
+*                                                            * 
+* DATE: 4/11/2021                                            *
+*                                                            * 
+* PURPOSE: Modularize F-16 C/D calculations for more         *
+*          complicated analyses                              *
+*                                                            * 
+* USAGE: Include header file in project and use the methods  *
+*        and attributes of the F16_CD object to obtain info  *
+*        on the F16 performance                              *
+*                                                            * 
+*************************************************************/
+  
